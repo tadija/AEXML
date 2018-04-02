d78bdadade9778fc683ec5c00cc5c9b5f793b4a7
Marko Tadic <tadija@me.com>
2018-04-03 00:26:57 +0200

    Updated license

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+/**
+ *  https://github.com/tadija/AEXML
+ *  Copyright (c) Marko Tadić 2014-2018
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
 import PackageDescription
 
 let package = Package(
-    name: "AEXML"
+    name: "AEXML",
+    exclude: ["Example"]
 )
